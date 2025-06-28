79f7a5e3feb779761b77691d4587e8803f87184a
milypol98 <milypol98@gmail.com>
2025-06-28 21:38:46 +0200

    rozpisanie widoku

--- src/main/java/com/milypol/security/car/Car.java
+++ src/main/java/com/milypol/security/car/Car.java
@@ -1,11 +1,10 @@
 package com.milypol.security.car;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.milypol.security.carCost.CarCost;
 import com.milypol.security.product.Product;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDate;
@@ -41,6 +40,9 @@ public class Car {
     )
     private List<Product> products;
 
-    @OneToMany
+    @OneToMany(mappedBy = "car", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
+    @JsonIgnore                    // ignoruj pole podczas serializacji JSON (REST, Thymeleaf)
+    @ToString.Exclude              // wyklucz z generowanego toString()
+    @EqualsAndHashCode.Exclude     // wyklucz z equals() i hashCode()
     private List<CarCost> costs;
 }
